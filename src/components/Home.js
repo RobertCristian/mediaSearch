1f816619d2ee3a2ff738e02306a4dad32ff2df59
Nagy Robert <robert.cristian@me.com>
2019-05-03 15:01:51 +0300

    Added route authorization protection

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,118 +1,134 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import "../css/App.css";
 import MovieRow from "./MovieRow";
 import SearchBar from "./SearchBar";
 import * as helpers from "../helpers/general";
-import { fetchMedia } from "../lib/fetchMedia";
+import {fetchMedia} from "../lib/fetchMedia";
 
-import { withFirebase } from "./Firebase";
+import {withFirebase} from "./Firebase";
+import {withAuthorization} from './Session';
 
 class Home extends Component {
-  constructor(props) {
-    super(props);
+    _isMounted = false;
 
-    this.state = {
-      media_type: "movie",
-      searchTerm: "",
-      loading: false,
-      users: []
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            media_type: "movie",
+            searchTerm: "",
+            loading: false,
+            users: []
+        };
+    }
+
+    componentWillMount() {
+        console.log('test');
+    }
+
+    componentDidMount() {
+        this._isMounted = true;
+        this.setState({loading: true});
+
+        if (this._isMounted) {
+            this.performSearch();
+        }
+
+        this.props.firebase.users().on("value", snapshot => {
+            const usersObject = snapshot.val();
+
+            console.log("usersobj", usersObject);
+
+            const usersList = Object.keys(usersObject).map(key => ({
+                ...usersObject[key],
+                uid: key
+            }));
+
+            console.log("userList", usersObject);
+
+            this.setState({
+                users: usersList,
+                loading: false
+            });
+        });
+    }
+
+    performSearch = helpers.debounce(searchTerm => {
+        const that = this;
+
+        if (this._isMounted) {
+            fetchMedia(searchTerm, this.state.media_type)
+                .then(function (data) {
+                    const results = data.results;
+
+                    let mediaRows = results.map(function (result) {
+                        if (result.poster_path) {
+                            result.poster_src =
+                                localStorage.getItem("image_base_url") +
+                                localStorage.getItem("poster_size") +
+                                result.poster_path;
+                        } else if (result.profile_path) {
+                            result.poster_src =
+                                localStorage.getItem("image_base_url") +
+                                localStorage.getItem("poster_size") +
+                                result.profile_path;
+                        }
+
+                        return (
+                            <MovieRow
+                                media_type={
+                                    result.media_type ? result.media_type : that.state.media_type
+                                }
+                                key={result.id}
+                                movie={result}
+                            />
+                        );
+                    });
+
+                    console.log(results);
+
+                    that.setState({mediaRows: mediaRows});
+                })
+                .catch(function (error) {
+                    console.log(
+                        "There has been a problem with your fetch operation: ",
+                        error.message
+                    );
+                });
+        }
+    }, 500);
+
+    searchChangeHandler = event => {
+        const searchTerm = event.target.value;
+
+        this.setState(
+            {
+                searchTerm: searchTerm
+            },
+            () => this.performSearch(this.state.searchTerm)
+        );
     };
 
-    this.performSearch();
-  }
+    componentWillUnmount() {
+        console.log('cucu2');
+        this.props.firebase.users().off();
+        this._isMounted = false;
+    }
 
-  componentDidMount() {
-    this.setState({ loading: true });
+    render() {
+        const {users} = this.state;
+        console.log(users);
 
-    this.props.firebase.users().on("value", snapshot => {
-      const usersObject = snapshot.val();
+        return (
+            <div>
+                <SearchBar searchChangeHandler={this.searchChangeHandler}/>
 
-      console.log("usersobj", usersObject);
-
-      const usersList = Object.keys(usersObject).map(key => ({
-        ...usersObject[key],
-        uid: key
-      }));
-
-      console.log("userList", usersObject);
-
-      this.setState({
-        users: usersList,
-        loading: false
-      });
-    });
-  }
-
-  componentWillUnmount() {
-    this.props.firebase.users().off();
-  }
-
-  performSearch = helpers.debounce(searchTerm => {
-    const that = this;
-
-    fetchMedia(searchTerm, this.state.media_type)
-      .then(function(data) {
-        const results = data.results;
-
-        let mediaRows = results.map(function(result) {
-          if (result.poster_path) {
-            result.poster_src =
-              localStorage.getItem("image_base_url") +
-              localStorage.getItem("poster_size") +
-              result.poster_path;
-          } else if (result.profile_path) {
-            result.poster_src =
-              localStorage.getItem("image_base_url") +
-              localStorage.getItem("poster_size") +
-              result.profile_path;
-          }
-
-          return (
-            <MovieRow
-              media_type={
-                result.media_type ? result.media_type : that.state.media_type
-              }
-              key={result.id}
-              movie={result}
-            />
-          );
-        });
-
-        console.log(results);
-
-        that.setState({ mediaRows: mediaRows });
-      })
-      .catch(function(error) {
-        console.log(
-          "There has been a problem with your fetch operation: ",
-          error.message
+                {this.state.mediaRows}
+            </div>
         );
-      });
-  }, 500);
-
-  searchChangeHandler = event => {
-    const searchTerm = event.target.value;
-
-    this.setState(
-      {
-        searchTerm: searchTerm
-      },
-      () => this.performSearch(this.state.searchTerm)
-    );
-  };
-
-  render() {
-    const { users } = this.state;
-    console.log(users);
-
-    return (
-      <div>
-        <SearchBar searchChangeHandler={this.searchChangeHandler} />
-
-        {this.state.mediaRows}
-      </div>
-    );
-  }
+    }
 }
 
-export default withFirebase(Home);
+const condition = authUser => !!authUser;
+
+export default withAuthorization(condition)(withFirebase(Home));
---
+++ src/components/Session/withAuthorization.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+
+import { withFirebase } from '../Firebase';
+
+const withAuthorization = condition => Component => {
+    class WithAuthorization extends React.Component {
+        componentDidMount() {
+            this.listener = this.props.firebase.auth.onAuthStateChanged(
+                authUser => {
+                    if (!condition(authUser)) {
+                        this.props.history.push('/login');
+                    }
+                },
+            );
+        }
+
+        componentWillUnmount() {
+            this.listener();
+        }
+
+        render() {
+            return (
+                <Component {...this.props} />
+            );
+        }
+    }
+
+    return withRouter(withFirebase(WithAuthorization));
+};
+
+export default withAuthorization;
